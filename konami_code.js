b0b8a84ec82051c8070957f6e940d5221d9c52ff
Erin Witmer <ecwitmer@gmail.com>
2017-12-15 19:23:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,10 +4,10 @@ function init() {
   let index = 0;
   document.body.addEventListener('keydown', function(e) {
     let expected = code[index]
-    let actual = parseInt(e.detail || e.which);
+    let actual = parseInt(e.which || e.detail);
     if (expected === actual) {
       index++
-      if (array.length === code.length) {
+      if (index === code.length) {
         alert('you won')
         index = 0;
       }
