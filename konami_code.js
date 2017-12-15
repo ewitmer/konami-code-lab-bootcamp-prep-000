6a2378b969e5a9c3c721e4dd2ed5d3323b4df795
Erin Witmer <ecwitmer@gmail.com>
2017-12-15 19:20:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   let index = 0;
-  document.onkeydown = function(e) {
+  document.body.onkeydown = function(e) {
     let expected = code[index]
     let actual = parseInt(e.detail || e.which);
     if (expected === actual) {
